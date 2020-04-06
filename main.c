33ebcb69330125f1f8b6de3b29d995eafb9a662c
Alessio Glorioso <alessioglorioso@MBP-de-Antonio.lan>
2020-04-07 01:19:58 +0200

    debut-echo

--- main.c
+++ main.c
@@ -4,18 +4,36 @@ int main()
 {
 	int bcl;
 	char *line;
+	int i;
+	int y;
+	char *echo;
+//	int ret;
 
 	bcl = 1;
+	y = 0;
 	line = NULL;
+	echo = NULL;
 	write(1,"\n",1);
 	write(1,"by Aglorios and Gverhelp\n",25);
 	write(1,"\n",1);
-	write(1,"#",1);
 	while(bcl == 1)
 	{
-		while (ft_get_next_line(0, &line))
+		write(1,"#",1);
+		while (get_next_line3d(0, &line) != 1)
 		{
 		}
+		if (ft_strncmp(&line[1], "echo", 4))
+		{
+			i = 5;
+			while(line[i])
+			{
+				write(1,&line[i],1);
+				i++;
+			}
+		}
+		else
+			write(1, "NOP", 4);
+		write(1,"\n",1);
 	}
 	return (0);
 }
