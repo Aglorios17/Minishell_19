76bb01bbcda970ade26d1791f330ad1fede1b7d6
gverhelp <garreth.verhelpen@gmail.com>
2020-12-08 17:45:14 +0100

    export end

--- export.c
+++ export.c
@@ -7,18 +7,13 @@ int ft_export(shell *st, char **envp)
 
     tmp = NULL;
     a = 0;
-//    printf("OK\n");
     if (!st->tokens->next)
         return (-1);
     st->tokens = st->tokens->next;
-//    printf("%s\n", envp[a]);
-    while (envp && envp[a])
-    {
-        tmp[a] = ft_strdup(envp[a]);
-//        printf("%s\n", envp[a]);
-        a++;
-        printf("%s\n", tmp[a]);
-    }
+    tmp = envp;
+    a = ft_strlen(tmp[a]);
+    tmp[a] = ft_strdup(st->tokens->content);
+    tmp[a + 1] = '\0';
     st->tokens = st->firsttok;
     return (0);
 }
