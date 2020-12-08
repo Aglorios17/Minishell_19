8466cb4024d501f58793262565c9cdb5ac779f56
Aglorios17 <alessio.glorioso17@gmail.com>
2020-12-08 19:38:41 +0100

    pierre-est-dans-le-vrai-?-

--- export.c
+++ export.c
@@ -16,11 +16,12 @@ int ft_export(shell *st, char **envp)
     if (!st->tokens->next)
         return (-1);
     st->tokens = st->tokens->next;
+//	tmp = malloc(sizeof(envp) + 1);
     tmp = envp;
     while (tmp[a])
         a++;
-    tmp[a] = ft_strdup(st->tokens->content);
-    tmp[a + 1] = '\0';
+    tmp[a] = ft_strdup((char *)st->tokens->content);
+    tmp[a + 1] = NULL;
     envp = tmp;
     st->tokens = st->firsttok;
     return (0);
